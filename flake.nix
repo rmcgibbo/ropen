4d8363cc056f3298215626fe0cc398499a90a5f7
Robert T. McGibbon <rmcgibbo@gmail.com>
2022-03-28 11:03:37 -0400

    [CI] Add naersk

--- flake.nix
+++ flake.nix
@@ -1,15 +1,19 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.utils.url = "github:numtide/flake-utils";
+  inputs.naersk = {
+    url = "github:nmattia/naersk";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, naersk }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
       in
       {
+        defaultPackage = naersk-lib.buildPackage { root = ./.; };
         devShell = pkgs.mkShell rec {
           buildInputs = with pkgs; [
             cargo
